26e02f024989e1efdc70e44fa04460ab6bfc3e64
Mark May <mamark90@gmail.com>
2019-05-11 10:41:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -305,16 +305,15 @@ def big_shoe_rebounds
 
             name = player
             shoe = player_stats[:shoe]
-            rebounds = player_stats[:rebounds]
             shoe_size.store(name, shoe)
-            shoe_size.store(:rebounds, rebounds)
             #new_hash creates a hash sorttd by shoe size (largest first) with a key of player' name
             new_hash = shoe_size.sort_by {|name, shoe| shoe}.reverse
+            binding.pry
             end
 
           end
         end
       end
 big_foot = new_hash.to_s
-  binding.pry
+  #binding.pry
   end
